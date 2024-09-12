8b5c4f2837580733b7d51288d7a56d09672e3956
bulutluoz <abbulutluoz@gmail.com>
2024-09-12 20:44:32 +0200

    day06 sonu

--- src/test/java/day06_jUnitFramework/C03_BeforeVeAfterNotasyonlari.java
+++ src/test/java/day06_jUnitFramework/C03_BeforeVeAfterNotasyonlari.java
@@ -1,12 +1,16 @@
 package day06_jUnitFramework;
 
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import utilities.ReusableMethods;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C03_BeforeVeAfterNotasyonlari {
 
@@ -35,7 +39,7 @@ public class C03_BeforeVeAfterNotasyonlari {
     // 2- phone icin arama yapin ve arama sonucunda urun bulunabildigini test edin
     // 3- bulunan urunlerden ilkini tiklayin
     //    ve acilan sayfadaki urun isminde case sensitive olmadan phone gectigini test edin
-
+    List<WebElement> bulunanUrunElementleriList;
     static WebDriver driver;
     @BeforeAll // her test method'undan once calis
     public static void setup(){
@@ -57,16 +61,58 @@ public class C03_BeforeVeAfterNotasyonlari {
     @Test
     public void test01(){
         // anaSayfaUrltesti
+        // 1- testotomasyonu anasayfaya gidin ve url'in testotomasyonu icerdigini test edin
+
+        driver.get("https://www.testotomasyonu.com");
+
+        String expectedUrlIcerik = "testotomasyonu";
+        String actualUrl = driver.getCurrentUrl();
+
+        if (actualUrl.contains(expectedUrlIcerik)){
+            System.out.println("Testotomasyonu url testi PASSED");
+        }else System.out.println("Testotomasyonu url testi FAILED");
+
     }
 
     @Test
     public void test02(){
         // phoneAramaTesti
+        // 2- phone icin arama yapin ve arama sonucunda urun bulunabildigini test edin
+
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+
+        aramaKutusu.sendKeys("phone" + Keys.ENTER );
+
+        // urun bulunabildigini test etmek icin
+        // bulunan tum urunleri bir listeye kaydedip size > 0 seklinde test edebiliriz
+
+        bulunanUrunElementleriList = driver.findElements(By.className("prod-img"));
+
+        if ( bulunanUrunElementleriList.size() > 0 ){
+            System.out.println("Urun bulabilme testi PASSED");
+        }else System.out.println("Urun bulabilme testi FAILED");
+
     }
 
     @Test
     public void test03(){
         // urunIsimTesti
+        // 3- bulunan urunlerden ilkini tiklayin
+        //    ve acilan sayfadaki urun isminde case sensitive olmadan phone gectigini test edin
+        bulunanUrunElementleriList = driver.findElements(By.className("prod-img"));
+        bulunanUrunElementleriList.get(0)
+                .click();
+
+        WebElement urunIsimElementi =
+                driver.findElement(By.xpath("//div[@class=' heading-sm mb-4']"));
+
+        String expectedUrunIsimIcerik = "phone";
+        String actualUrunIsmiKucukHarf = urunIsimElementi.getText().toLowerCase();
+
+        if (actualUrunIsmiKucukHarf.contains(expectedUrunIsimIcerik)){
+            System.out.println("Urun isim icerik testi PASSED");
+        }else System.out.println("Urun isim icerik testi FAILED");
+
     }
 
 }
