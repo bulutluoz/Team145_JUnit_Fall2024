c8173e830a545f730b32e0cdbf027a7661fb8ad5
bulutluoz <abbulutluoz@gmail.com>
2024-09-13 19:53:06 +0200

    day07 sonu

--- src/test/java/day07_/C01_AssertionVeTeardown.java
+++
@@ -1,9 +0,0 @@
-package day07_;
-
-public class C01_AssertionVeTeardown {
-
-    // Yapilan islem tek bir test method'unda yapilacak kadar kucuk olsa da
-    // teardown() kullanmak daha mantiklidir
-
-
-}
