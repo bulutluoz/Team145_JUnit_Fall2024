e6f0af60b023bc5c2650d1713a161568736d8d63
bulutluoz <abbulutluoz@gmail.com>
2024-09-16 21:44:28 +0200

    day08 sonu

--- src/test/java/day08_JsAlerts_Iframe/C05_iFrame.java
+++ src/test/java/day08_JsAlerts_Iframe/C05_iFrame.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import utilities.ReusableMethods;
 import utilities.TestBase_Each;
 
+import java.util.List;
+
 public class C05_iFrame extends TestBase_Each {
 
     @Test
@@ -40,11 +42,64 @@ public class C05_iFrame extends TestBase_Each {
 
         Assertions.assertEquals(expectedDellUrunIsim,actualDellUrunIsim);
 
+
+        // 5- sag bolumde bolum ismi olan "Fashion" yazisinin gorunur oldugunu test edin
+        // Fashion yazisi sag tarafta bulunan BASKA BIR iframe'in icinde
+
+        // biz yukarda (25.satir) electronics iFrame'ine gecis yapmistik
+        // Fashion iFrame'ine gecmekl icin once
+        // electronics iFrame'inden anasayfaya cikmaliyiz
+
+        driver.switchTo().defaultContent();
+        // eger ic ice birden fazla iFrame varsa en ustteki anasayfaya goturur
+        // driver.switchTo().parentFrame();
+        // eger ic ice birden fazla iFrame varsa bir ustteki iFrame'e goturur
+
+
+        // anasayfaya ciktik, artik fashion iFrame'ine gecis yapabiliriz
+
+        WebElement fashionIFrame =
+                driver.findElement(By.xpath("(//iframe[@frameborder='1'])[2]"));
+
+        driver.switchTo().frame(fashionIFrame);
+
+        ReusableMethods.bekle(1);
+
+        WebElement fashionBolumYaziElementi =
+                driver.findElement(By.xpath("//h2[text() = 'Fashion']"));
+
+        Assertions.assertTrue(fashionBolumYaziElementi.isDisplayed());
+
         //4- Sagdaki bolumde gorunen urunler arasinda
         //   ‘Men Slim Fit’ içeren en az 1 urun olduğunu test edin
-        //5- ‘Fashion’ yazisinin gorunur olduğunu test edin
+
+        List<WebElement> urunIsimElementleriList = driver.findElements(By.tagName("p"));
+
+        int sayac = 0;
+
+        for(WebElement eachElement : urunIsimElementleriList ){
+
+            if (eachElement.getText().contains("Men Slim Fit")){
+                sayac++;
+            }
+        }
+
+        Assertions.assertTrue(sayac>0);
+
         //6- ‘Here are some products’ yazisinin gorunur olduğunu test edin
 
-        ReusableMethods.bekle(3);
+        // bu yazi elementi anasayfada oldugundan
+        // once fashionIFrame'inden anasayfaya cikmaliyiz
+
+        driver.switchTo().defaultContent();
+        ReusableMethods.bekle(1);
+
+        WebElement hereAreSomeYaziElementi =
+                driver.findElement(By.xpath("//*[text()='Here are some products.']"));
+
+        Assertions.assertTrue(hereAreSomeYaziElementi.isDisplayed());
+
+
+        ReusableMethods.bekle(1);
     }
 }
