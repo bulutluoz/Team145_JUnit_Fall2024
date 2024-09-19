42bac687d81c555f34d80bf73e94a0294aab712f
bulutluoz <abbulutluoz@gmail.com>
2024-09-19 21:49:10 +0200

    Ders11 sonu

--- src/test/java/day11_fileTestleri_Waits/C05_ExplicitlyWait.java
+++ src/test/java/day11_fileTestleri_Waits/C05_ExplicitlyWait.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.ReusableMethods;
 
 import java.time.Duration;
@@ -85,13 +87,36 @@ public class C05_ExplicitlyWait {
     public void explicitWaitTest(){
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         //    1. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
         //    2. Textbox’in etkin olmadigini(enabled) dogrulayın
-        //    3. Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
+        WebElement textBox = driver.findElement(By.xpath("//input[@type='text']"));
+        Assertions.assertFalse(textBox.isEnabled());
+        //    3. Enable butonuna tıklayın
+        driver.findElement(By.xpath("//*[text()='Enable']"))
+                .click();
+
+        //    ve textbox etkin oluncaya kadar bekleyin
+            // 1- Bir wait objesi olustur
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+            // 2- Kullanacagimiz WebElementi locate edin
+            //    bu soru icin kullanilacak textBox zaten yukarda locate edilip, kaydedildi
+
+            // 3- wait objesi ile belirlenmis bekleme senaryolarindan uygun olani secin
+        wait.until(ExpectedConditions.elementToBeClickable(textBox));
+
         //    4. Textbox’in etkin oldugunu(enabled) dogrulayın.
+        Assertions.assertTrue(textBox.isEnabled());
+
         //    5. “It’s enabled!” mesajinin goruntulendigini dogrulayın.
+        WebElement mesajElementi = driver.findElement(By.id("message"));
+
+        String expectedYazi = "It's enabled!";
+        String actualYazi = mesajElementi.getText();
+
+        Assertions.assertEquals(expectedYazi,actualYazi);
 
     }
 
